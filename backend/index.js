e023796a17040f6ca204dfda9976091af33258ff
parth <parthrk75@gmail.com>
2023-10-13 16:38:51 +0530

    add list-favorite

--- backend/index.js
+++ backend/index.js
@@ -23,22 +23,33 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/list-favorite', (req, res) => {
+  const sql = 'SELECT * FROM favorites';
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error retrieving favorite movies: ' + err);
+      res.status(500).send('Error retrieving favorite movies.');
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
 
 app.post('/mark-as-favorite', (req, res) => {
-    const movieId = req.body.movieId; 
-  
-    const sql = 'INSERT INTO favorites (movie_id) VALUES (?)';
-    db.query(sql, [movieId], (err, result) => {
-      if (err) {
-        console.error('Error marking the movie as a favorite: ' + err);
-        res.status(500).send('Error marking the movie as a favorite.');
-      } else {
-        console.log('Movie marked as a favorite.');
-        res.status(200).send('Movie marked as a favorite.');
-      }
-    });
+  const movieId = req.body.movieId;
+
+  const sql = 'INSERT INTO favorites (movie_id) VALUES (?)';
+  db.query(sql, [movieId], (err, result) => {
+    if (err) {
+      console.error('Error marking the movie as a favorite: ' + err);
+      res.status(500).send('Error marking the movie as a favorite.');
+    } else {
+      console.log('Movie marked as a favorite.');
+      res.status(200).send('Movie marked as a favorite.');
+    }
   });
-  
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
